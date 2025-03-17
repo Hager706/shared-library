207038b3750453101a67f4c89d011cd104a30031
Hager706 <hagertarek706@gmail.com>
2025-03-17 14:58:01 +0200

    2

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,8 +1,11 @@
+
 def call(String namespace, String kubeTokenCredId) {
     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
         sh """
-            kubectl config set-credentials jenkins-user --token=${KUBE_TOKEN}
-            kubectl config set-context --current --user=jenkins-user --namespace=${namespace}
+            # No need to create a new config, just use the existing one
+            # and switch to the right namespace
+            kubectl config use-context minikube
+            kubectl config set-context --current --namespace=${namespace}
             kubectl apply -f k8s-deployment.yaml
         """
     }
