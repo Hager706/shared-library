087fc51078925791395341a7e2a91e130ae42c51
Hager706 <hagertarek706@gmail.com>
2025-03-17 16:40:16 +0200

    2

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,30 +1,13 @@
+
 def call(String namespace, String kubeTokenCredId) {
     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
         sh """
-            # Create a kubeconfig file with the correct local port
-            cat > kubeconfig-${namespace} << EOF
-            apiVersion: v1
-            kind: Config
-            clusters:
-            - name: minikube
-              cluster:
-                server: https://127.0.0.1:58815
-                insecure-skip-tls-verify: true
-            users:
-            - name: jenkins-user
-              user:
-                token: ${KUBE_TOKEN}
-            contexts:
-            - name: jenkins-context
-              context:
-                cluster: minikube
-                user: jenkins-user
-                namespace: ${namespace}
-            current-context: jenkins-context
-            EOF
-            
-            # Apply the deployment using the correct port
-            kubectl --kubeconfig=kubeconfig-${namespace} apply -f k8s-deployment.yaml
+            # No need to create a new config, just use the existing one
+            # and switch to the right namespace
+            kubectl config use-context minikube
+            kubectl config set-context --current --namespace=${namespace}
+            kubectl apply -f k8s-deployment.yaml
         """
     }
 }
+   
