11d37735f296b6402ff8e4eaca46ba07d127be2e
Hager706 <hagertarek706@gmail.com>
2025-03-17 19:21:22 +0200

    push

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,26 +1,12 @@
-def call(String namespace, String kubeTokenCredId, String kubernetesApiServer, String deploymentFile) {
+
+def call(String namespace, String kubeTokenCredId) {
     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
         sh """
-            # Debugging: Print Kubernetes API server address
-            echo "Kubernetes API Server: ${kubernetesApiServer}"
 
-            # Configure Kubernetes credentials
-            kubectl config set-credentials jenkins-user --token=${KUBE_TOKEN}
-
-            # Configure Kubernetes cluster
-            kubectl config set-cluster kubernetes --server=${kubernetesApiServer} --insecure-skip-tls-verify=true
-
-            # Create and set a new context
-            kubectl config set-context jenkins-context --cluster=kubernetes --user=jenkins-user --namespace=${namespace}
-            kubectl config use-context jenkins-context
-
-            # Debugging: Verify connectivity to the Kubernetes API server
-            echo "Testing connectivity to the Kubernetes API server..."
-            curl -k ${kubernetesApiServer}
-
-            # Apply the deployment
-            echo "Applying deployment file: ${deploymentFile}"
-            kubectl apply -f ${deploymentFile} --validate=false
+            kubectl config use-context minikube
+            kubectl config set-context --current --namespace=${namespace}
+            kubectl apply -f k8s-deployment.yaml
         """
     }
 }
+   
