f9d4bce7498e03951dbdbb0161543f67fa3988d4
Hager706 <hagertarek706@gmail.com>
2025-03-17 17:26:06 +0200

    2

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,12 +1,30 @@
 
-def call(String namespace, String kubeTokenCredId) {
+// def call(String namespace, String kubeTokenCredId) {
+//     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
+//         sh """
+
+//             kubectl config use-context minikube
+//             kubectl config set-context --current --namespace=${namespace}
+//             kubectl apply -f k8s-deployment.yaml
+//         """
+//     }
+// }
+
+def call(String namespace, String kubeTokenCredId, String kubernetesApiServer, String deploymentFile) {
     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
         sh """
-            # No need to create a new config, just use the existing one
-            # and switch to the right namespace
-            kubectl config use-context minikube
-            kubectl config set-context --current --namespace=${namespace}
-            kubectl apply -f k8s-deployment.yaml
+            # Configure Kubernetes credentials
+            kubectl config set-credentials jenkins-user --token=${KUBE_TOKEN}
+
+            # Configure Kubernetes cluster
+            kubectl config set-cluster kubernetes --server=${kubernetesApiServer} --insecure-skip-tls-verify=true
+
+            # Create and set a new context
+            kubectl config set-context jenkins-context --cluster=kubernetes --user=jenkins-user --namespace=${namespace}
+            kubectl config use-context jenkins-context
+
+            # Apply the deployment
+            kubectl apply -f ${deploymentFile}
         """
     }
 }
