ffb1b0f6a5dc238654f7d2786b395753a5e347f1
Hager706 <hagertarek706@gmail.com>
2025-03-18 08:59:23 +0200

    lab1

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -10,23 +10,45 @@
 //     }
 // }
    
-   def call(String namespace, String kubeTokenCredId) {
+//    def call(String namespace, String kubeTokenCredId) {
+//     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
+//         sh """
+//             # Configure kubectl auth
+//             kubectl config set-credentials jenkins --token=${KUBE_TOKEN}
+//             kubectl config set-context minikube --user=jenkins
+            
+//             kubectl config use-context minikube
+//             kubectl config set-context --current --namespace=${namespace}
+            
+//             # Replace the image with the correct tag
+//             sed -i 's|hagert/multi-app|hagert/multi-app:${namespace}|g' k8s-deployment.yaml
+            
+//             kubectl apply -f k8s-deployment.yaml
+            
+//             # Restore the original file
+//             git checkout -- k8s-deployment.yaml || true
+//         """
+//     }
+// }
+def call(String namespace, String kubeTokenCredId) {
     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
+        // Use a more secure way to pass the token
         sh """
             # Configure kubectl auth
-            kubectl config set-credentials jenkins --token=${KUBE_TOKEN}
+            kubectl config set-credentials jenkins --token=\${KUBE_TOKEN}
             kubectl config set-context minikube --user=jenkins
             
             kubectl config use-context minikube
             kubectl config set-context --current --namespace=${namespace}
             
-            # Replace the image with the correct tag
-            sed -i 's|hagert/multi-app|hagert/multi-app:${namespace}|g' k8s-deployment.yaml
+            # Create a temporary file with the correct image tag
+            cat k8s-deployment.yaml | sed "s|hagert/multi-app|hagert/multi-app:${namespace}|g" > temp-deployment.yaml
             
-            kubectl apply -f k8s-deployment.yaml
+            # Apply the temporary file
+            kubectl apply -f temp-deployment.yaml
             
-            # Restore the original file
-            git checkout -- k8s-deployment.yaml || true
+            # Clean up
+            rm temp-deployment.yaml
         """
     }
 }
