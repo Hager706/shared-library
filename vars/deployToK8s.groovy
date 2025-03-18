e46b58b1f786d50cb4aced7e2a86169523c49b3b
Hager706 <hagertarek706@gmail.com>
2025-03-18 08:55:47 +0200

    lab1

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,12 +1,32 @@
 
-def call(String namespace, String kubeTokenCredId) {
+// def call(String namespace, String kubeTokenCredId) {
+//     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
+//         sh """
+
+//             kubectl config use-context minikube
+//             kubectl config set-context --current --namespace=${namespace}
+//             kubectl apply -f k8s-deployment.yaml
+//         """
+//     }
+// }
+   
+   def call(String namespace, String kubeTokenCredId) {
     withCredentials([string(credentialsId: kubeTokenCredId, variable: 'KUBE_TOKEN')]) {
         sh """
-
+            # Configure kubectl auth
+            kubectl config set-credentials jenkins --token=${KUBE_TOKEN}
+            kubectl config set-context minikube --user=jenkins
+            
             kubectl config use-context minikube
             kubectl config set-context --current --namespace=${namespace}
+            
+            # Replace the image with the correct tag
+            sed -i 's|hagert/multi-app|hagert/multi-app:${namespace}|g' k8s-deployment.yaml
+            
             kubectl apply -f k8s-deployment.yaml
+            
+            # Restore the original file
+            git checkout -- k8s-deployment.yaml || true
         """
     }
 }
-   
